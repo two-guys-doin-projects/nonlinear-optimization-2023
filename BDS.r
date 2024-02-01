f115d0559338a9d80b2bc1d4ab6a4c86d68778e6
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-02-01 13:31:06 +0100

    update BDS via Flip

--- BDS.r
+++ BDS.r
@@ -34,11 +34,12 @@ BDS <- function(f, x0, step){
   while(TRUE){
     next_x_a <- x0 - 2 ** iterator * step
     next_x_b <- x0 + 2 ** iterator * step
-    if(f(next_x_a) >= f(x0) & found_a == FALSE){
+    #cat('i: ', iterator, ' || found_a: ', found_a, ' || found_b: ', found_b, ' || f(x): ', f(x0), ' || f(next_x): ', f(next_x_a), '\n')
+    if((f(next_x_a) >= f(x0) || f(next_x_a) == -Inf || f(next_x_a) == Inf) & found_a == FALSE){
       a <- next_x_a
       found_a <- TRUE
     }
-    if(f(next_x_b) >= f(x0) & found_b == FALSE){
+    if((f(next_x_b) >= f(x0) || f(next_x_b) == -Inf || f(next_x_b) == Inf) & found_b == FALSE){
       b <- next_x_b
       found_b <- TRUE
     }
